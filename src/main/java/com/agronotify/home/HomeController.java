b3be9fb3ef7d7e8bb3d9e471eaa0492195cd2154
Tutzdev <arthurasw05@gmail.com>
2026-06-30 00:18:51 -0300

    feat: deploy inicial do Agro Notify na VPS

--- src/main/java/com/agronotify/home/HomeController.java
+++ src/main/java/com/agronotify/home/HomeController.java
@@ -1,13 +1,13 @@
 package com.agronotify.home;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Agro Notify rodando";
+        return "redirect:/login.html";
     }
 }
